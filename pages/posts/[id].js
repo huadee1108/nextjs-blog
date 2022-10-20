19f1ec47e48d8e339f2b2796342be0dcefa0cfac
huadee1108 <981929654@qq.com>
2022-10-20 19:21:32 +0800

    ssr

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,16 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
+        <meta
+          name="description"
+          content="Learn how to build a ssg-ssr"
+        />
+        <meta
+          property="og:image"
+          content={`https://sograph-static.s3.ap-southeast-1.amazonaws…board/images/7B2B2E1CB3F1899C9E6D5100782792B0.png`}
+        />
+        <meta name="og:title" content={'ssg-ssr'} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
